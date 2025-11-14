f6bb919b1865b65fc2d3e5370cf8be2c7856f45d
lookpy <144287412+lookpy@users.noreply.github.com>
2025-11-14 23:35:32 +0000

    55

--- app/src/main/java/com/cdccreditsmart/app/presentation/screens/home/ModernHomeScreen.kt
+++ app/src/main/java/com/cdccreditsmart/app/presentation/screens/home/ModernHomeScreen.kt
@@ -2,6 +2,7 @@ package com.cdccreditsmart.app.presentation.screens.home
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
